f62c6299314eaf047efba9303c4041f0720c6ab3
Buck Chancey <roland-id-au@users.noreply.github.com>
2025-12-02 01:20:06 +1000

    Fix: Update cloudflare_record to use 'content' instead of deprecated 'value'

--- infra/cloudflare.tf
+++ infra/cloudflare.tf
@@ -2,7 +2,7 @@
 resource "cloudflare_record" "api_subdomain" {
   zone_id = var.cloudflare_zone_id
   name    = "api.labs" // Cloudflare manages labs.teleports.cloud directly
-  value   = "labs-teleports-cloud.onrender.com"
+  content = "labs-teleports-cloud.onrender.com"
   type    = "CNAME"
   proxied = false
   ttl     = 1
